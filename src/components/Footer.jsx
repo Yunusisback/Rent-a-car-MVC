24a2700f6a8512bacedea0bd34782d70fe65e2a3
YunusDev <71130477+Yunusisback@users.noreply.github.com>
2025-10-26 21:50:24 +0300

    Localize footer content with translation keys

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+// src/components/Footer.jsx
 import { useApp } from '../context/AppContext';
 import { useTranslation } from '../utils/translations';
 import { Mail, MapPin, Phone, Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
@@ -10,18 +11,12 @@ export function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-
         {/* Üst Bölüm - Kolonlar */}
         <div className="footer-columns">
-
           {/* Kolon 1 - Hakkımızda */}
-          
           <div className="footer-column">
-            <h3 className="footer-title">KeyGo</h3>
-            <p className="footer-text">
-              Avrupa genelinde premium araç kiralama hizmetleri sunuyoruz. 
-              Güvenilir, konforlu ve lüks araçlarla seyahat edin.
-            </p>
+            <h3 className="footer-title">{t.title}</h3>
+            <p className="footer-text">{t.aboutDescription}</p>
             <div className="social-links">
               <a href="#" className="social-link" aria-label="Facebook">
                 <Facebook size={20} />
@@ -39,67 +34,61 @@ export function Footer() {
           </div>
 
           {/* Kolon 2 - Hizmetler */}
-
           <div className="footer-column">
-            <h4 className="footer-heading">Hizmetler</h4>
+            <h4 className="footer-heading">{t.services}</h4>
             <ul className="footer-links">
-              <li><a href="#">Araç Kiralama</a></li>
-              <li><a href="#">Kurumsal Çözümler</a></li>
-              <li><a href="#">Havalimanı Transferi</a></li>
-              <li><a href="#">Uzun Dönem Kiralama</a></li>
-              <li><a href="#">Mobil Uygulama</a></li>
+              <li><a href="#">{t.carRental}</a></li>
+              <li><a href="#">{t.corporateSolutions}</a></li>
+              <li><a href="#">{t.airportTransfer}</a></li>
+              <li><a href="#">{t.longTermRental}</a></li>
+              <li><a href="#">{t.mobileApp}</a></li>
             </ul>
           </div>
 
           {/* Kolon 3 - Yasal */}
-
           <div className="footer-column">
-            <h4 className="footer-heading">Yasal Bilgiler</h4>
+            <h4 className="footer-heading">{t.legalInfo}</h4>
             <ul className="footer-links">
-              <li><a href="#">Kullanım Koşulları</a></li>
-              <li><a href="#">Gizlilik Politikası</a></li>
-              <li><a href="#">Çerez Politikası</a></li>
-              <li><a href="#">KVKK</a></li>
-              <li><a href="#">İptal ve İade</a></li>
+              <li><a href="#">{t.termsOfUse}</a></li>
+              <li><a href="#">{t.privacyPolicy}</a></li>
+              <li><a href="#">{t.cookiePolicy}</a></li>
+              <li><a href="#">{t.kvkk}</a></li>
+              <li><a href="#">{t.cancellationRefund}</a></li>
             </ul>
           </div>
 
           {/* Kolon 4 - İletişim */}
-
           <div className="footer-column">
-            <h4 className="footer-heading">İletişim</h4>
+            <h4 className="footer-heading">{t.contact}</h4>
             <ul className="footer-contact">
               <li>
                 <MapPin size={18} />
-                <span>İstanbul, Türkiye</span>
+                <span>{t.location}</span>
               </li>
               <li>
                 <Phone size={18} />
-                <span>+90 555 123 4567</span>
+                <span>{t.phone}</span>
               </li>
               <li>
                 <Mail size={18} />
-                <span>info@keygo.eu</span>
+                <span>{t.email}</span>
               </li>
             </ul>
           </div>
         </div>
 
         {/* Alt bölüm - copyright */}
-
         <div className="footer-bottom">
           <div className="footer-bottom-content">
-            <p className="copyright">
-              © KeyGo 2025 
-            </p>
+            <p className="copyright">{t.copyright}</p>
             <div className="footer-bottom-links">
-              <a href="#">Site Haritası</a>
+              <a href="#">{t.siteMap}</a>
               <span className="separator">•</span>
-              <a href="#">Yasal Uyarı</a>
+              <a href="#">{t.legalNotice}</a>
               <span className="separator">•</span>
-              <a href="#">Erişilebilirlik</a>
+              <a href="#">{t.accessibility}</a>
               <span className="separator">•</span>
-              <a href="#">Bize Ulaşın</a>
+              <a href="#">{t.contactUs}</a>
             </div>
           </div>
         </div>
