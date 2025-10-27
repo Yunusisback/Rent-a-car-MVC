7b63a4dca549235784234c6606a8c54a1da63ad7
Yunusisback <yunusyertum1@gmail.com>
2025-10-27 23:57:56 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+// vitest.config.js
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/tests/setup.js',
+    environment: 'jsdom', 
+    setupFiles: './src/tests/setup.js', 
+    globals: true, 
+    include: ['src/tests/**/*.{test,spec}.{js,jsx,ts,tsx}'], 
   },
 });
